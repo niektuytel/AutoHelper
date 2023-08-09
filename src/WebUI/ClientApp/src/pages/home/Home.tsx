c3421b9dbd5efd8ec9b0b612c70f12ec1c676d1e
niektuytel <46424968+niektuytel@users.noreply.github.com>
2023-08-09 16:54:43 +0200

    Wip Building location search.

--- src/WebUI/ClientApp/src/pages/home/Home.tsx
+++ src/WebUI/ClientApp/src/pages/home/Home.tsx
@@ -1,17 +1,17 @@
 ﻿import React from "react";
-import { BottomNavigation, Box, Breadcrumbs, Button, ButtonGroup, Chip, Container, Grid, Hidden, Link, Typography } from "@material-ui/core";
-import NextWeekOutlinedIcon from '@material-ui/icons/NextWeekOutlined';
+import { Box, Container, InputAdornment, TextField, IconButton, Button, Hidden } from "@material-ui/core";
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import SearchIcon from '@material-ui/icons/Search';
+import ClearIcon from '@material-ui/icons/Clear';
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router";
 
+
 // local
-import { GotoProductsIconStyle, GotoProductsStyle } from "./HomeStyle";
-import NavSection from "./components/NavSection";
-import About from "./sections/about/About";
-import Information from "./sections/InformationSection";
 import Faq from "./sections/ContactSection";
 import { HashValues } from "../../i18n/HashValues";
 import ReactPlayer from "react-player";
+
 import TypedLogo, { TypedIconStyle } from "../../components/logo/TypedLogo";
 import ImageLogo from "../../components/logo/ImageLogo";
 import { colorOnIndex } from "../../i18n/ColorValues";
@@ -27,61 +27,111 @@ export default ({isAdmin}:IProps) => {
     const splitted_hash = hash.split("_")[0];
     const { t } = useTranslation();
 
+    const [suggestions, setSuggestions] = React.useState<string[]>([]);
+    const [inputValue, setInputValue] = React.useState<string>("");
+
+    const handleClearInput = () => {
+        setInputValue('');
+    };
+    /// GOOGLE MAPS API: AIzaSyDHOHHAjYZsfPQqy7qQaopZNcLWJMp6Re0
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+        // Here, you can also fetch and update the suggestions based on the input value.
+    };
+
+    const handleSearch = () => {
+        // Handle the search functionality here
+        // For now, let's just console.log the input value
+        console.log(inputValue);
+    }
+
+
     return <>
-        <div style={{width:"100%", position:"absolute"}}>
-            <img 
-                src="https://images.pexels.com/photos/3806249/pexels-photo-3806249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
-                style={{
-                    height:"100%",    
-                    margin: "auto",
-                    position: "absolute",
-                    left: "0",
-                    right: "0"
-                }}
-            />
-        </div>
-        <Box 
+        <Box
             style={{
-                // height:"90vh",
-                position:"relative",
-                marginLeft:"10px",
-                marginRight:"10px"
+                position: "relative",
+                marginLeft: "10px",
+                marginRight: "10px"
             }}
         >
-            <Box
-                className="noselect"
+            <Container
+                maxWidth="lg"
                 style={{
-                    zIndex: 1,
-                    cursor: "pointer",
-                    width: "100%",
-                    height: "65vh"
+                    padding: "0",
+                    textAlign: "center"
                 }}
-                onClick={() => history.push("/products")}
             >
-                <Container 
-                    maxWidth="lg" 
+                <Box
                     style={{
-                        padding:"0", 
-                        textAlign:"center"
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center'
                     }}
                 >
-                    <Box
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center'
+                    <TextField
+                        fullWidth
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        variant="outlined"
+                        placeholder="Adres, b.v. Amstelplein 10"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <LocationOnOutlinedIcon color="action" />
+                                </InputAdornment>
+                            ),
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton onClick={handleClearInput}>
+                                        <ClearIcon color="action" />
+                                    </IconButton>
+                                    <Hidden xlDown implementation="css">
+                                        <IconButton onClick={handleSearch}>
+                                            <SearchIcon />
+                                        </IconButton>
+                                    </Hidden>
+                                    <Hidden lgUp implementation="css">
+                                        {suggestions.length > 1 && (
+                                            <Button
+                                                onClick={handleSearch}
+                                                style={{
+                                                    backgroundColor: '#1B97F0',
+                                                    color: 'white',
+                                                    borderRadius: '4px',
+                                                    height: '100%',
+                                                    borderTopLeftRadius: '0',
+                                                    borderBottomLeftRadius: '0',
+                                                    margin: '0',
+                                                    padding: '0',
+                                                }}
+                                            >
+                                                Search
+                                            </Button>
+                                        )}
+                                    </Hidden>
+                                </InputAdornment>
+                            ),
+                            style: {
+                                height: '50px',
+                                fontSize: '1.2em',
+                                paddingRight: '0',
+                            }
                         }}
-                    >
-                        <Typography 
-                            // variant={"subtitle2"} 
-                            className={classes.root}
-                            style={GotoProductsStyle}
-                        >
-                            {t('see_products')}
-                        </Typography>
-                    </Box>
-                </Container>
-            </Box>
+                    />
+                    {
+                        inputValue && (
+                            <Box mt={2} boxShadow={3} style={{ maxHeight: '200px', overflowY: 'auto', backgroundColor: '#fff' }}>
+                                {suggestions.map((suggestion, index) => (
+                                    <Box key={index} p={1} borderBottom="1px solid #ddd">
+                                        {suggestion}
+                                    </Box>
+                                ))}
+                            </Box>
+                        )
+                    }
+                </Box>
+            </Container>
         </Box>
     </>
 }
