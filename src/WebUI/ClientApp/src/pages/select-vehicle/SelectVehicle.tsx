0f8267cc5af152ea7ab751e096e5028781c624c2
niektuytel <46424968+niektuytel@users.noreply.github.com>
2023-09-26 09:22:25 +0200

    Clean select vehicle page

--- src/WebUI/ClientApp/src/pages/select-vehicle/SelectVehicle.tsx
+++ src/WebUI/ClientApp/src/pages/select-vehicle/SelectVehicle.tsx
@@ -20,54 +20,31 @@ import "slick-carousel/slick/slick-theme.css";
 import { useTranslation } from "react-i18next";
 import { useNavigate, useParams } from "react-router-dom";
 
-// local
-import { HashValues } from "../../i18n/HashValues";
-import TextFieldLicensePlates from "./components/LicensePlateTextField";
+// own imports
 import GradientBox from "./components/GradientBox";
 import LicensePlateHeadView from "./views/LicensePlateSearchView";
 import AutoHelperAboutView from "./views/AutoHelperAboutView";
 import PlacesHeadView from "./views/PlacesSearchView";
-
-
-import VehicleHeaderBar from "./components/VehicleHeaderBar";
 import VehicleInfoView from "./views/VehicleInfoView";
 import { ROUTES } from "../../constants/routes";
-
-interface StyledProps {
-    theme: Theme;
-}
-
-const DemoPaper = styled(Paper)<StyledProps>(({ theme }) => ({
-    width: 120,
-    height: 120,
-    padding: theme.spacing(2),
-    ...theme.typography.body2,
-    textAlign: 'center',
-}));
 
 interface IProps {}
 
 
 export default ({ }: IProps) => {
     const { licence_plate } = useParams();
-    const hash = window.location.hash.length == 0 ? HashValues.select_vehicle_default : window.location.hash;
-
-    const showOnMaintanance = (licence_plate && hash == HashValues.select_vehicle_maintanance);
-    const showOnInfo = (licence_plate && hash == HashValues.select_vehicle_info);
 
     return (
         <>
-            {!showOnInfo && 
-                <GradientBox>
-                    <Container maxWidth="lg" sx={{ padding: "0", textAlign: "center" }}>
-                        {showOnMaintanance ?
-                            <PlacesHeadView licence_plate={licence_plate || ""}/>
-                            :
-                            <LicensePlateHeadView/>
-                        }
-                    </Container>
-                </GradientBox>
-            }
+            <GradientBox>
+                <Container maxWidth="lg" sx={{ padding: "0", textAlign: "center" }}>
+                    { window.location.pathname === ROUTES.SELECT_VEHICLE ?
+                        <LicensePlateHeadView/>
+                        :
+                        <PlacesHeadView licence_plate={licence_plate || ""}/>
+                    }
+                </Container>
+            </GradientBox>
             <Container maxWidth="lg">
                 { window.location.pathname === ROUTES.SELECT_VEHICLE ? 
                     <AutoHelperAboutView />
